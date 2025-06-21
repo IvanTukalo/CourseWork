0c1d1acf2dc4772a64c413bd9b2853e1d59a53ff
IvanTukalo <tukalo.ivan1123@vu.cdu.edu.ua>
2025-06-21 20:23:16 +0300

    the order of launching windows has been changed added global resorses

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace CourseWork
+{
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 1. Створюємо та показуємо сплеш-скрін
+            SplashScreenWindow splashScreen = new SplashScreenWindow();
+            splashScreen.Show();
+
+            // 2. Створюємо головне вікно, але поки не показуємо його
+            MainWindow mainWindow = new MainWindow();
+
+            // 3. Підписуємось на подію закриття сплеш-скріна
+            splashScreen.Closed += (s, args) =>
+            {
+                // 4. Коли сплеш-скрін закрився, показуємо головне вікно
+                mainWindow.Show();
+            };
+        }
+    }
+}
